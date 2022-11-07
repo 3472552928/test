3dfcc910e0f113ceeb3943e065ece228b12040c6
3472552928 <110899482+3472552928@users.noreply.github.com>
2022-11-07 13:47:56 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
-# import pytest
-# import os
+import pytest
+import os
 
-# # 运行测试报告
-# if __name__ == '__main__':
-#     # 1、查找所有测试报告用例并执行
-#     pytest.main(["-s",
-#                  "--alluredir=report/allure_report","--clean-alluredir"])
-#     # 2、把测试报告生成html（第一种方式）  serve启动
-#     # os.system(r"allure serve ./allure_raw")
-#     # 3、把测试报告生成html   generate生成报告    clean删除报告
-#     os.system("allure generate report/allure_report -o report/report_html --clean")
+# 运行测试报告
+if __name__ == '__main__':
+    # 1、查找所有测试报告用例并执行
+    pytest.main(["-s",
+                 "--alluredir=report/allure_report","--clean-alluredir"])
+    # 2、把测试报告生成html（第一种方式）  serve启动
+    # os.system(r"allure serve ./allure_raw")
+    # 3、把测试报告生成html   generate生成报告    clean删除报告
+    os.system("allure generate report/allure_report -o report/report_html --clean")
 
